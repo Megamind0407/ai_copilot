5b0cc6b36f7850b44513d533dbfb3cde32853f16
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2026-04-10 00:03:41 +0530

    update main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.analyze import router as analyze_router
 
 # Create FastAPI application instance
@@ -8,13 +9,26 @@ app = FastAPI(
     version="1.0.0"
 )
 
+origins = [
+    "http://localhost:5173",   # Vite default
+    "http://127.0.0.1:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,   # or ["*"] for testing
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Root endpoint (basic health message)
 @app.get("/")
 def read_root():
     return {
         "status": "ok",
-        "message": "Backend server is running successfully 🚀"
+        "message": "Backend server is running successfully"
     }
 
 
